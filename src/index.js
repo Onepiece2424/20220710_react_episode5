54ce3a2e75854a60df25a2a05414c8614d7b93b0
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-10 11:07:29 +0900

    作成したコンポーネントファイルをcomponentsフォルダへ移動

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom";
-// import { CssModules } from "./CssModules";
-// import { StyledJsx } from "./StyledJsx";
-import { StyledComponents } from "./StyledComponents";
+import { CssModules } from "./CssModules";
+// import { StyledJsx } from "./components/StyledJsx";
+// import { StyledComponents } from "./components/StyledComponents";
 
-// ReactDOM.render(<CssModules />, document.getElementById("root"));
+ReactDOM.render(<CssModules />, document.getElementById("root"));
 // ReactDOM.render(<StyledJsx />, document.getElementById("root"));
-ReactDOM.render(<StyledComponents />, document.getElementById("root"));
+// ReactDOM.render(<StyledComponents />, document.getElementById("root"));
