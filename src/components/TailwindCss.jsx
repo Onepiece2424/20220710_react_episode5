d31bc073acabd15eb0a3c7430e56c1afe568841c
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-10 12:01:17 +0900

    TailwindCssの使用

--- src/components/TailwindCss.jsx
+++ src/components/TailwindCss.jsx
@@ -1,8 +1,10 @@
 export const TailwindCss = () => {
   return (
-    <div>
-      <p>Tailwind Css です。</p>
-      <button>ボタン</button>
+    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
+      <p className="m-0 text-gray-400">Tailwind Css です。</p>
+      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gra-400 hober:text-white">
+        ボタン
+      </button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom";
 // import { CssModules } from "./CssModules";
 // import { StyledJsx } from "./components/StyledJsx";
 // import { StyledComponents } from "./components/StyledComponents";
-import { Emotion } from "./components/Emotion";
+// import { Emotion } from "./components/Emotion";
+import { TailwindCss } from "./components/TailwindCss";
 
 // ReactDOM.render(<CssModules />, document.getElementById("root"));
 // ReactDOM.render(<StyledJsx />, document.getElementById("root"));
 // ReactDOM.render(<StyledComponents />, document.getElementById("root"));
-ReactDOM.render(<Emotion />, document.getElementById("root"));
+// ReactDOM.render(<Emotion />, document.getElementById("root"));
+ReactDOM.render(<TailwindCss />, document.getElementById("root"));
